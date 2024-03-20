7b30a438725936a4da616391b8919805a4c8b823
emeze36@gmail.com <emeze36@gmail.com>
2024-03-20 12:23:40 +0100

    first version of array callBack

--- week-3/callback.js
+++ week-3/callback.js
@@ -1,5 +1,21 @@
-function sayHello(){
-    return console.log("Hello");
+// function sayHello(){
+//     return console.log("Hello");
 
+// }
+// setInterval(sayHello, 1000);
+
+const myNumbers = [4, 1, -20, -7, 5, 9, -6];
+
+const positiveNumbers = removeNegative(myNumbers, (x) => x >= 0);
+
+document.getElementById("callback").innerHTML = positiveNumbers;
+
+function removeNegative(numbers,callback) {
+    const myArray = [];
+    for (const x of numbers) {
+        if (callback(x)) {
+            myArray.push(x);
+        }
+    }
+    return myArray;
 }
-setInterval(sayHello, 1000)
